5f35d06c74acaa9f5caedd2f133c154ef44b3a92
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-11 17:02:51 +0000

    game system added (no game content)

--- js/game.js
+++ js/game.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha. Vorontsov
+// Created on: April 11th
 // This file contains the JS functions for index.html
+
+/**
+ * Start Phaser Game
+ */
+
+  const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    length: 1080,
+    backgroundColor:0x5f6e7a
+  }
+
+  const game = new Phaser.Game (config)
+  console.log(game)
