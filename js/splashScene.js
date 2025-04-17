a2eb75c67b73e82e4e2a74563eb410615cbb00bc
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-17 16:49:25 +0000

    Added Splash>js

--- js/splashScene.js
+++ js/splashScene.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Sasha. Vorontsov
-// Created on: April 11th
+// Created on: April 17th
 // This file contains the JS functions for index.html
+
+class SplashScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'splashScene' })
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+  }
+
+  create (data) {
+  }
+
+  update (time,delta) {
+  }
+}
+
+export default SplashScene
