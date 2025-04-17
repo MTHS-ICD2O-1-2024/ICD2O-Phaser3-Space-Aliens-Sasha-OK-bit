f0df2c0a6dc34d4e7db3af3b28a719043fb38985
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-17 16:37:41 +0000

    Added new splash title screen and added more coding on game.js

--- js/game.js
+++ js/game.js
@@ -4,6 +4,10 @@
 // Created on: April 11th
 // This file contains the JS functions for index.html
 
+import SplashScene from './splashScene.js'
+
+// Our game scene
+const splashScene = new SplashScene()
 /**
  * Start Phaser Game
  */
@@ -12,8 +16,19 @@
     type: Phaser.AUTO,
     width: 1920,
     length: 1080,
-    backgroundColor:0x5f6e7a
+    backgroundColor:0x5f6e7a,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      //we place it in the middle of the page
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    }
   }
 
-  const game = new Phaser.Game (config)
-  console.log(game)
+const game = new Phaser.Game (config)
+
+// load scenes
+// NOTE: remeber any "key" is global and CAN NOT be reused
+game.scene.add('splashScene', splashScene)
+
+//start title
+game.scene.start('splashScene')
---
+++ js/splashScene.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Sasha. Vorontsov
+// Created on: April 11th
+// This file contains the JS functions for index.html
