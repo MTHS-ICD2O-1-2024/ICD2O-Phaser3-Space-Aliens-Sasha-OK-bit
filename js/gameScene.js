77cd46c72a58cb0058028d02117c6e926367a359
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-26 16:39:59 +0000

    done 13

--- js/gameScene.js
+++ js/gameScene.js
@@ -37,6 +37,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("alien", "assets/alien.png")
     // sound
     this.load.audio("laser", "assets/laser1.wav")
+    this.load.audio("explosion", "assets/barrelExploding.wav")
   }
 
   create (data) {
@@ -53,6 +54,9 @@ class GameScene extends Phaser.Scene {
     this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
       alienCollide.destroy()
       missileCollide.destroy()
+      this.sound.play("explosion")
+      this.createAlien()
+      this.createAlien()
     }.bind(this))
   }  
 
